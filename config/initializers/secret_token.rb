a85c0559c73761ade18f4bb41998cc670358a958
Paul Arterburn <paul@brandfolder.com>
2014-09-21 13:28:42 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OhlifeExport::Application.config.secret_key_base = '6d715a133cfa96489533acfce2969ab1dd5b79acfd1b34d5d874a2daee43841599d892d966cb0d37a913f6f9335f9ab2a1aa9014cb61d842a3625bd61b4e7059'
